8f2df600ef74328719688032cbeb8841f1a46c9d
faten <fatenkhoualdia@gmail.com>
2023-10-23 21:42:37 +0200

    fix bug

--- 02-basic-challenges/02-array-intersection/array-intersection.js
+++ 02-basic-challenges/02-array-intersection/array-intersection.js
@@ -1,1 +1,20 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+    const arr3 = [];
+  
+    for (const item of arr1) {
+        if(arr2.includes(item) && !arr3.includes(item)){
+        arr3.push(item);
+      }
+    }
+  
+    return arr3
+  }
+  
+  const result1 = arrayIntersection([1, 2, 3, 4, 5], [1, 3, 5, 7, 9])
+  console.log(result1)
+
+  const result2 = arrayIntersection([1, 1, 1, 1, 1], [2, 2, 2, 2, 2])
+  console.log(result2)
+
+const result3= arrayIntersection([1, 2, 3, 4, 5], [5, 4, 3, 2, 1])
+console.log(result3)
--- 02-basic-challenges/03-display-likes/display-likes.js
+++ 02-basic-challenges/03-display-likes/display-likes.js
@@ -1,1 +1,33 @@
-function displayLikes() {}
+function displayLikes(likesArray=[]) {
+    const length = likesArray.length
+    switch (length) {
+        case 0 :
+           return  'no one likes this'
+                  
+        case 1 :
+          return   ` ${likesArray[0]}likes this`
+        case 2:
+            return      ` ${likesArray[0]} , ${likesArray[1]} like this}`   
+        case 3:
+               return    `${likesArray[0]},  ${likesArray[1]} ,and ${likesArray[2]}like this}` 
+
+          
+        default:
+        return ` ${likesArray[0]},${likesArray[1]}, and ${likesArray.length - 2} others like this`
+
+      }
+
+
+
+    }
+
+  
+ console.log(displayLikes([])) // 'no one likes this'
+ console.log(displayLikes(['Peter'])) // 'Peter likes this'
+ console.log(displayLikes(['Jacob', 'Alex'])) // 'Jacob and Alex like this'
+ console.log(displayLikes(['Max', 'John', 'Mark'])) // 'Max, John and Mark like this'
+ console.log(displayLikes(['Alex', 'Jacob', 'Mark', 'Max'])) // 'Alex, Jacob and 2 others like this'
+ console.log(displayLikes(['Alex', 'Jacob', 'Mark', 'Max', 'Jill']) )// 'Alex, Jacob and 3 others like this'
+
+  
+  
